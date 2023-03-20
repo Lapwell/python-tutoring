903da990d32ef3f29c88cc0fa4c329310b32d798
Byron.C <byronrwcor@protonmail.com>
2023-03-20 07:06:46 -0700

    b

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,9 @@ import { Portfolio } from "./components/Portfolio";
 export default function Home() {
   return(
   <Stack styles={{width:'100%', height:'100%'}}>
-    <AboutMe/>
+    {/* <AboutMe/>
     <Schedule/>
-    <Portfolio/>
+    <Portfolio/> */}
   </Stack>
   )
 }
